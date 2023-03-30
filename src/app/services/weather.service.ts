9a023d7017ce724dc27eda4e0fa47645c093dc81
a1dnan <aaliladnane@gmail.com>
2023-03-30 23:44:20 +0000

    delete environment directory and file

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,7 +1,6 @@
 import { WeatherData } from './../models/weather.model';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 
 @Injectable({
