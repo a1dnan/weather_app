9bbc60881a0d1a338b58c4d13cf10ec4b0c11dda
a1dnan <aaliladnane@gmail.com>
2023-03-31 02:51:59 +0000

    Update weather service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -8,24 +8,15 @@ import { Observable } from 'rxjs';
 })
 export class WeatherService {
 
-  headers = {
-    weatherUrl: 'https://openweather43.p.rapidapi.com/weather',
-    XRapidAPIKeyName : 'X-RapidAPI-Key',
-    XRapidAPIKeyValue : 'ce9f912023msha4aa1af26f87736p184500jsn41a0e855f08f',
-    XRapidAPIHostName : 'X-RapidAPI-Host',
-    XRapidAPIHostValue : 'openweather43.p.rapidapi.com'
-  };
+  apiUrl = 'https://api.openweathermap.org/data/2.5/weather'
 
   constructor(private http: HttpClient) { }
 
   getWeatherData(city: string): Observable<WeatherData>{
-    return this.http.get<WeatherData>(this.headers.weatherUrl, {
-      headers: new HttpHeaders()
-      .set(this.headers.XRapidAPIHostName, this.headers.XRapidAPIHostValue)
-      .set(this.headers.XRapidAPIKeyName, this.headers.XRapidAPIKeyValue),
+    return this.http.get<WeatherData>(this.apiUrl, {
       params: new HttpParams()
       .set('q', city)
-      .set('appid','da0f9c8d90bde7e619c3ec47766a42f4')
+      .set('appid','268fec76d80429be5a686a8aae239599')
       .set('units','metric')
 
     })
